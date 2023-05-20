ad152bbf06833c84a56743e5f30581a7b4c9768d
Mozart409 <amadeus@mozart409.com>
2023-05-20 18:06:18 +0200

    feat: :tada: init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,40 @@
+import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { Footer } from "~/ui/footer";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <div className="bg-zinc-50">
+      <h1 className="text-3xl font-bold">Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+      <Footer />
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{js,ts,tsx,md,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
